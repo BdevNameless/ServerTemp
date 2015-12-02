e7cc936446f5d0bc496b78ca0429bfaf425ba2be
BdevNameless <bdev.nameless@gmail.com>
2015-12-02 12:20:31 +1000

    + VPN Connection Tested + If protocol config already exists, manager uses it.

--- ServerTemp/TestConnectionViewController.swift
+++ ServerTemp/TestConnectionViewController.swift
@@ -15,7 +15,13 @@ class TestConnectionViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var connButton: UIButton!
     @IBAction func connButtonTapped(sender: UIButton) {
-        
+        do {
+            try NEVPNManager.sharedManager().connection.startVPNTunnel()
+        } catch let error {
+            textView.text = textView.text + "\n>НЕ УДАЛОСЬ УСТАНОВИТЬ СОЕДИНЕНИЕ. ERROR CODE: \(error)."
+            return
+        }
+        textView.text = textView.text + "\n>ПОДКЛЮЧЕНИЕ УСПЕШНО УСТАНОВЛЕНО."
     }
     
     //MARK: - Lifecycle
@@ -23,6 +29,14 @@ class TestConnectionViewController: UIViewController {
         super.viewDidLoad()
         configureView()
         confugureVPN()
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        let manager = NEVPNManager.sharedManager()
+        if manager.connection.status == .Connected {
+            manager.connection.stopVPNTunnel()
+        }
     }
     
     private func configureView() {
@@ -44,44 +58,48 @@ class TestConnectionViewController: UIViewController {
                 self.textView.text = self.textView.text + "\n>\(error)"
             }
             else{
-                self.textView.text = self.textView.text + "\n>VPN MANAGER УСПЕШНО ЗАГРУЖЕН.\n>НАСТРАИВАЮ ПОДКЛЮЧЕНИЕ..."
-                let configs = VPNConfiguration()
-                let p = NEVPNProtocolIPSec()
-                p.username = configs.username
-                self.textView.text = self.textView.text + "\n>ИСПОЛЬЗУЮ ИМЯ ПОЛЬЗОВАТЕЛЯ: \(p.username)."
-                let pwdRef = configs.getPersistentRefForPassword()
-                if pwdRef.status == nil {
-                    p.passwordReference = pwdRef.0
-                    self.textView.text = self.textView.text + "\n>ПОЛУЧЕН УКАЗАТЕЛЬ НА ПАРОЛЬ ИЗ СВЯЗКИ"
-                }
-                else{
-                    self.textView.text = self.textView.text + "\n>НЕ УДАЛОСЬ ПОЛУЧИТЬ УКАЗАТЕЛЬ НА ПАРОЛЬ ИЗ СВЯЗКИ. ERROR CODE: \(pwdRef.status)."
-                }
-                p.serverAddress = configs.serverAddress
-                self.textView.text = self.textView.text + "\n>ИСПОЛЬЗУЮ АДРЕС СЕРВЕРА: \(p.serverAddress)."
-                p.authenticationMethod = .SharedSecret
-                let shsRef = configs.getPersistentRefForSharedKey()
-                if shsRef.status == nil {
-                    p.sharedSecretReference = shsRef.0
-                    self.textView.text = self.textView.text + "\n>ПОЛУЧЕН УКАЗАТЕЛЬ НА ОБЩИЙ СЕКРЕТ ИЗ СВЯЗКИ"
-                }
-                else{
-                    self.textView.text = self.textView.text + "\n>НЕ УДАЛОСЬ ПОЛУЧИТЬ УКАЗАТЕЛЬ НА ОБЩИЙ СЕКРЕТ ИЗ СВЯЗКИ. ERROR CODE: \(shsRef.status)."
-                }
-                p.disconnectOnSleep = false
-                p.useExtendedAuthentication = true
-                manager.protocolConfiguration = p
-                manager.localizedDescription = "МОЙ УЮТНЫЙ VPN"
-                self.textView.text = self.textView.text + "\n>ПРОТОКОЛ СКОНФИГУРИРОВАН. СОХРАНЯЮ НАСТРОЙКИ..."
-                manager.saveToPreferencesWithCompletionHandler() { [unowned self] (error: NSError?) in
-                    if error != nil {
-                        self.textView.text = self.textView.text + "\n>\(error)"
+                if manager.protocolConfiguration == nil {
+                    self.textView.text = self.textView.text + "\n>VPN MANAGER УСПЕШНО ЗАГРУЖЕН.\n>НАСТРАИВАЮ ПОДКЛЮЧЕНИЕ..."
+                    let configs = VPNConfiguration()
+                    let p = NEVPNProtocolIPSec()
+                    p.username = configs.username
+                    self.textView.text = self.textView.text + "\n>ИСПОЛЬЗУЮ ИМЯ ПОЛЬЗОВАТЕЛЯ: \(p.username)."
+                    let pwdRef = configs.getPersistentRefForPassword()
+                    if pwdRef.status == nil {
+                        p.passwordReference = pwdRef.0
+                        self.textView.text = self.textView.text + "\n>ПОЛУЧЕН УКАЗАТЕЛЬ НА ПАРОЛЬ ИЗ СВЯЗКИ"
                     }
                     else{
-                        self.textView.text = self.textView.text + "\nНАСТРОЙКИ УСПЕШНО СОХРАНЕНЫ.\n>⬆︎JUST TAP THIS FCKING BUTTON⬆︎"
+                        self.textView.text = self.textView.text + "\n>НЕ УДАЛОСЬ ПОЛУЧИТЬ УКАЗАТЕЛЬ НА ПАРОЛЬ ИЗ СВЯЗКИ. ERROR CODE: \(pwdRef.status)."
+                    }
+                    p.serverAddress = configs.serverAddress
+                    self.textView.text = self.textView.text + "\n>ИСПОЛЬЗУЮ АДРЕС СЕРВЕРА: \(p.serverAddress)."
+                    p.authenticationMethod = .SharedSecret
+                    let shsRef = configs.getPersistentRefForSharedKey()
+                    if shsRef.status == nil {
+                        p.sharedSecretReference = shsRef.0
+                        self.textView.text = self.textView.text + "\n>ПОЛУЧЕН УКАЗАТЕЛЬ НА ОБЩИЙ СЕКРЕТ ИЗ СВЯЗКИ"
+                    }
+                    else{
+                        self.textView.text = self.textView.text + "\n>НЕ УДАЛОСЬ ПОЛУЧИТЬ УКАЗАТЕЛЬ НА ОБЩИЙ СЕКРЕТ ИЗ СВЯЗКИ. ERROR CODE: \(shsRef.status)."
+                    }
+                    p.disconnectOnSleep = false
+                    p.useExtendedAuthentication = true
+                    manager.protocolConfiguration = p
+                    manager.localizedDescription = "МОЙ УЮТНЫЙ VPN"
+                    self.textView.text = self.textView.text + "\n>ПРОТОКОЛ СКОНФИГУРИРОВАН. СОХРАНЯЮ НАСТРОЙКИ..."
+                    manager.saveToPreferencesWithCompletionHandler() { [unowned self] (error: NSError?) in
+                        if error != nil {
+                            self.textView.text = self.textView.text + "\n>\(error)"
+                        }
+                        else{
+                            self.textView.text = self.textView.text + "\n>НАСТРОЙКИ УСПЕШНО СОХРАНЕНЫ.\n>⬆︎JUST TAP THIS FCKING BUTTON⬆︎"
+                        }
                     }
                 }
-                
+                else {
+                    self.textView.text = self.textView.text + "\n>ЗАГРУЖЕНЫ СУЩЕСТВУЮЩИЕ НАСТРОЙКИ.\n>⬆︎JUST TAP THIS FCKING BUTTON⬆︎"
+                }
             }
         }
     }
