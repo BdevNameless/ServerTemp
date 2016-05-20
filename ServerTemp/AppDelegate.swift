0c1532f8de29bee1035403469c0a9bcb8a42331f
BdevNameless <bdev.nameless@gmail.com>
2016-05-20 11:51:31 +1000

    little fix

--- ServerTemp/AppDelegate.swift
+++ ServerTemp/AppDelegate.swift
@@ -79,7 +79,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             if loadError == nil {
                 dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(0.1*Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
                     try! config.startVPN()
-//                    config.restartVPN()
                 }
             }
         }
